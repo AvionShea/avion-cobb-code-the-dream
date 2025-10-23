965c4e549008a126b2f3964eef501b7662967ebb
Avion C. <83898002+AvionShea@users.noreply.github.com>
2025-10-23 18:27:34 -0400

    fix: rename function to populateFoodPics and update call to match

--- Intro To Programming - Luna/Fetch-API/fetch.js
+++ Intro To Programming - Luna/Fetch-API/fetch.js
@@ -1,6 +1,6 @@
 const body = document.body;
 
-async function populateInfo() {
+async function populateFoodPics() {
     const requestHTML = "https://foodish-api.com/api/";
 
     const request = new Request(requestHTML);
@@ -17,4 +17,28 @@ async function populateInfo() {
     body.appendChild(img);
 }
 
-populateInfo();
+populateFoodPics();
+
+async function getNarutoPic() {
+    const url = "https://api.jikan.moe/v4/anime?q=narut";
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            const img = document.createElement("img");
+            const imgSrc = response.data[0].images.jpg.image_url;
+
+            img.src = imgSrc;
+            img.alt = "pic of Naruto"
+
+            body.appendChild(img);
+        })
+        .catch(function () {
+            console.log("404 - Not Found");
+        })
+}
+
+getNarutoPic();
+
